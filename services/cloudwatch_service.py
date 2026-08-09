070d3858fb22d209622278ddafff7b078c93a9a3
Preetham Manigonda <manigondapreetham@gmail.com>
2026-08-09 22:40:37 +0530

    Add network CloudWatch metrics

--- services/cloudwatch_service.py
+++ services/cloudwatch_service.py
@@ -2,18 +2,29 @@ import boto3
 from datetime import datetime, timedelta, timezone
 
 
-def get_cpu_utilization(instance_id):
-    cloudwatch = boto3.client(
+REGION = "ap-south-2"
+
+
+def get_cloudwatch_client():
+    return boto3.client(
         "cloudwatch",
-        region_name="ap-south-2",
+        region_name=REGION,
     )
+
+
+def get_metric_statistics(
+    instance_id,
+    metric_name,
+    statistic="Average",
+):
+    cloudwatch = get_cloudwatch_client()
 
     end_time = datetime.now(timezone.utc)
     start_time = end_time - timedelta(minutes=10)
 
     response = cloudwatch.get_metric_statistics(
         Namespace="AWS/EC2",
-        MetricName="CPUUtilization",
+        MetricName=metric_name,
         Dimensions=[
             {
                 "Name": "InstanceId",
@@ -23,7 +34,7 @@ def get_cpu_utilization(instance_id):
         StartTime=start_time,
         EndTime=end_time,
         Period=300,
-        Statistics=["Average"],
+        Statistics=[statistic],
     )
 
     datapoints = response.get("Datapoints", [])
@@ -36,4 +47,28 @@ def get_cpu_utilization(instance_id):
         key=lambda datapoint: datapoint["Timestamp"],
     )
 
-    return round(latest_datapoint["Average"], 2)
+    return round(latest_datapoint[statistic], 2)
+
+
+def get_cpu_utilization(instance_id):
+    return get_metric_statistics(
+        instance_id,
+        "CPUUtilization",
+        "Average",
+    )
+
+
+def get_network_in(instance_id):
+    return get_metric_statistics(
+        instance_id,
+        "NetworkIn",
+        "Average",
+    )
+
+
+def get_network_out(instance_id):
+    return get_metric_statistics(
+        instance_id,
+        "NetworkOut",
+        "Average",
+    )
