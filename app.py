d539c993084ac2b3b731312d608a62d906982512
Preetham Manigonda <manigondapreetham@gmail.com>
2026-08-09 12:12:44 +0530

    Connect Flask app to EC2 service

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 
+from services.ec2_service import get_ec2_instances
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "CloudOps Monitor is running!"
 
-if __name__ == '__main__':
+@app.route("/")
+def home():
+    instances = get_ec2_instances()
+
+    return {
+        "application": "CloudOps Monitor",
+        "instances": instances,
+    }
+
+
+if __name__ == "__main__":
     app.run(debug=True)
