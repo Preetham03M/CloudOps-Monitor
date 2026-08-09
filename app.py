4df7d4fef2aac9588b0221ef5eab5f8707ffcbaf
Preetham Manigonda <manigondapreetham@gmail.com>
2026-08-09 13:52:49 +0530

    Integrate CloudWatch CPU metrics

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 
 from services.ec2_service import get_ec2_instances
+from services.cloudwatch_service import get_cpu_utilization
 
 app = Flask(__name__)
 
@@ -8,6 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     instances = get_ec2_instances()
+
+    for instance in instances:
+        instance["cpu"] = get_cpu_utilization(instance["instance_id"])
 
     running_count = sum(
         1 for instance in instances if instance["state"] == "running"
